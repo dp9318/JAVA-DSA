f1f894f25503c25d663f5c0cee805aa32e292f08
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-11-20 00:47:59 +0530

    commit_20_11_25_01

---
+++ Basics/variables_in.java
@@ -0,0 +1,17 @@
+public class variables_in {
+    public static void main(String[] args) {
+        int integer=10;
+        char character = 'A';
+        String string = "Java is fun!";
+        Boolean bool = true;
+        float decimal = 43.32f;
+        double bigDecimal = 234.234232;
+
+        System.out.println("Integer = " + integer);
+        System.out.println("Char = " + character);
+        System.out.println("String = " + string);
+        System.out.println("Boolean = " + bool);
+        System.out.println("Float = " + decimal);
+        System.out.println("Double = " + bigDecimal);
+    }
+}
