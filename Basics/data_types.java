799d8af5b1e82d2fc9da7cc10c886f47a33ef8ab
Dipankar Kumar <dipankarkumar626@gmail.com>
2025-12-26 14:44:27 +0530

    basics in java

--- Basics/data_types.java
+++ Basics/data_types.java
@@ -1,6 +1,20 @@
 public class data_types {
-
     public static void main(String[] args) {
-        
+        System.out.println("Integer = " + Integer.BYTES + " bytes");
+        System.out.println("Char = " + Character.BYTES + " bytes");
+        System.out.println("Float = " + Float.BYTES + " bytes");
+        System.out.println("Double = " + Double.BYTES + " bytes");
+
+        // Java doesn't define fixed size for these:
+        System.out.println("Boolean = JVM dependent");
+        System.out.println("String = depends on string length + char size");
+
+
+        // constant variables can be written with initials start with final keyword
+        final int myroll = 9318;
+
+        // myroll = myroll + 10; this is not legal in JAVA as myroll variable is a constant variable with final keyword
+
+        System.out.println(myroll);
     }
 }
---
+++ Basics/typecasting.java
@@ -0,0 +1,15 @@
+public class typecasting {
+    public static void main(String[] args) {
+        int num = 65;
+        System.out.println(num);
+
+        char ch = 'A';
+        System.out.println(ch);
+        
+        int character = ch;
+        System.out.println(character);
+
+
+    }
+    
+}
