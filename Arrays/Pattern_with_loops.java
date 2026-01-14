b57686998d5631e1ee65de0b5de26a9e25533ffb
Dipankar Kumar <dipankarkumar626@gmail.com>
2026-01-14 11:58:17 +0530

    Operators in java

---
+++ Arrays/Pattern_with_loops.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.Scanner;
+
+public class Pattern_with_loops {
+    public static void main(String[] args) {
+        //printing a suare in java 
+        System.out.println("This is a square from the user input:");
+        Scanner sc;
+        int n;
+
+    }
+}
---
+++ Basics/operators.java
@@ -0,0 +1,90 @@
+public class operators {
+    public static void main(String[] args) {
+        // Operators in JAVA
+
+        // Arithmetic operators
+        /*
+        
+        op          name                operation 
+        +	        Addition		    x + y	
+        -	        Subtraction	        x - y	
+        *	        Multiplication	    x * y	
+        /	        Division		    x / y	
+        %	        Modulus	            x % y	
+        ++	        Increment		    ++x	 (it is preincrement, postincrement : x++)
+        --	        Decrement	        --x  (it is predecrement, postdecrement : x--)
+
+        */
+
+        //All the operations using the arithmetic operators follow BODMAS rule
+
+
+        // Assignment operator
+
+        /*
+
+        op          operation
+        =	        x = 5	
+        +=	    	x = x + 3	
+        -=	    	x = x - 3	
+        *=	    	x = x * 3	
+        /=	    	x = x / 3	
+        %=	    	x = x % 3	
+        &=	    	x = x & 3	
+        |=	    	x = x | 3	
+        ^=	    	x = x ^ 3	
+        >>=	    	x = x >> 3	
+        <<=	    	x = x << 3
+
+        */
+
+
+
+        // Comparison operators
+
+        /* The operators which are used in conditions are here:
+        
+        op      name                        operation
+        ==	    Equal to	                x == y	
+        !=	    Not equal	                x != y	
+        >	    Greater than	            x > y	
+        <	    Less than	                x < y	
+        >=	    Greater than or equal to	x >= y	
+        <=	    Less than or equal to	    x <= y
+        
+        */
+
+
+        // Logical operators
+
+        /*
+        op      name                                operation
+        && 	    Logical and	Returns true if 
+                both statements are true	        x < 5 &&  x < 10
+
+        || 	    Logical or	Returns true if 
+                one of the statements is true	    x < 5 || x < 4	
+
+        !	    Logical not	Reverse the result,returns 
+                false if the result is true	        !(x < 5 && x < 10)
+         */
+
+
+
+        //Precidence of Operators
+
+        /*
+        symbol              operator name
+        ()                  Parentheses
+        *, /, %             Multiplication, Division, Modulus
+        +, -                Addition, Subtraction
+        >, <, >=, <=        Comparison
+        ==, !=              Equality
+        &&                  Logical AND
+        ||                  Logical OR
+        =                   Assignment
+
+        up == high precidence
+        */
+    }
+}
