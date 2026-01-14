fea5be72401c61ec17185f5b4eb9435653a18fd2
Dipankar Kumar <dipankarkumar626@gmail.com>
2026-01-14 12:09:08 +0530

    strings, gitignore added

---
+++ Basics/Strings.java
@@ -0,0 +1,13 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Dipankar";
+
+        int len = name.length();
+
+        System.out.println(name+ " name's length = " + len);
+
+        System.out.println("uppercasing the name = "+ name.toUpperCase());
+        System.out.println("lolwercasing the name = "+ name.toLowerCase());
+        
+    }
+}
