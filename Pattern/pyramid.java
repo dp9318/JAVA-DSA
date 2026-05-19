b029bc1cdd5a4338bb4c92a690805b42a6144bba
Dipankar Kumar <dipankarkumar626@gmail.com>
2026-05-19 23:48:11 +0530

    more arrays question in java

---
+++ Pattern/pyramid.java
@@ -0,0 +1,13 @@
+public class pyramid {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=i; j<5; j++){
+                System.out.print(" ");
+            }
+            for(int p=0; p<i*2-1; p++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern/reverse_pyramid.java
@@ -0,0 +1,13 @@
+public class reverse_pyramid {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            for(int p=9; p>i*2; p--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
