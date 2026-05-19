beea987171f8d85b3336ff315df7d6b889562a3c
Dipankar Kumar <dipankarkumar626@gmail.com>
2026-05-20 00:42:59 +0530

    o-1 triangle problem arrays

--- Pattern/diamond.java
+++ Pattern/diamond.java
@@ -1,13 +1,21 @@
 public class diamond {
     public static void main(String[] args) {
-        for(int i=0; i<10; i++){
+        for(int i=0; i<=5; i++){
             for(int j=i; j<5; j++){
                 System.out.print(" ");
             }
             for(int p=0; p<i*2-1; p++){
                 System.out.print("*");
             }
-            
+            System.out.println();
+        }
+        for(int i=0; i<=5; i++){
+            for(int p=0; p<i; p++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<10-i*2-1; j++){
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
